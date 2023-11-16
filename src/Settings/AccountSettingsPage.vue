01d744a644382980c3d29f1f54415b6ca9f4f328
Avoo <git@avoonix.com>
2023-11-16 21:49:23 +0100

    Added "verify credentials" button to account settings

--- src/Settings/AccountSettingsPage.vue
+++ src/Settings/AccountSettingsPage.vue
@@ -7,11 +7,23 @@
           <v-text-field filled label="e621 username" type="text" v-model="username" autocomplete="username" />
           <v-text-field filled :append-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
             :type="showPassword ? 'text' : 'password'" label="e621 API key" v-model="apiKey"
-            @click:append="showPassword = !showPassword" autocomplete="password" />
+            @click:append="showPassword = !showPassword" autocomplete="password" counter="24" />
           <p class="text-left">
             Go to <external-link :href="`${e621Url}users/home`" /> > Manage API Access to get the API key
           </p>
-          <v-btn :disabled="!username" color="accent" text @click="toggleFavoritesMenuItem">
+          <v-btn :disabled="!username || !apiKey" :loading="verification.loading" :color="verification.success ? 'success' : verification.message ? 'error' : 'accent'" text @click="verifyCredentials">
+              Verify credentials
+          </v-btn>
+          <p v-if="verification.message">
+              {{ verification.message }}
+          </p>
+          <p class="text-left" v-if="!verification.success && verification.message">
+              A network error means that <i>something</i> did not work. 
+              Most likely, this was an authentication error. Double check if the username is exactly the same as on <external-link :href="`${e621Url}users/home`" /> and make sure you copied the API key correctly - it should be 24 characters long.
+              <br />
+              Due to a security policy (CORS), Material e621 cannot determine the cause of the error. There might be a general error with the network or e621.
+          </p>
+          <v-btn class="mt-4" :disabled="!username" color="accent" text @click="toggleFavoritesMenuItem">
             {{ usernameSavedSearchExists ? `Remove "Favorites" saved search` : `Add "Favorites" saved search` }}
           </v-btn>
         </settings-page-item>
@@ -35,10 +47,12 @@
 <script lang="ts">
 import SettingsPageTitle from "./SettingsPageTitle.vue";
 import SettingsPageItem from "./SettingsPageItem.vue";
-import { computed, defineComponent, ref } from "vue";
+import { computed, defineComponent, ref, watch } from "vue";
 import ExternalLink from "@/App/ExternalLink.vue";
 import { useAccountStore, useSavedSearchStore, useUrlStore } from "@/services";
 import { SavedSearchEntry } from "@/services/types";
+import { getApiService } from "@/worker/services";
+import { BlacklistMode } from "@/services/types";
 
 export default defineComponent({
   metaInfo: {
@@ -105,6 +119,43 @@ export default defineComponent({
       }
     }
 
+    const verification = ref({
+        success: false,
+        loading: false,
+        message: "",
+    });
+    const verifyCredentials = async () => {
+        verification.value.loading = true;
+        try {
+            const service = await getApiService();
+            const posts = await service.getPosts({
+              limit: 1,
+              tags: ["rating:s"],
+              blacklist: [],
+              blacklistMode: BlacklistMode.hide,
+              auth: account.auth,
+              baseUrl: url.e621Url
+            });
+            verification.value.success = true;
+            verification.value.message = "Credentials are valid";
+        } catch (e: any) {
+            console.dir(e);
+            verification.value.success = false;
+            verification.value.message = `Credentials are invalid: ${e.message || e}`;
+        } finally {
+            verification.value.loading = false;
+        }
+    };
+
+    watch(username, () => {
+      verification.value.message = "";
+      verification.value.success = false;
+    });
+    watch(apiKey, () => {
+      verification.value.message = "";
+      verification.value.success = false;
+    });
+
     return {
       toggleFavoritesMenuItem,
       usernameSavedSearchExists,
@@ -113,6 +164,8 @@ export default defineComponent({
       apiKey,
       e621Url,
       proxyUrl,
+      verification,
+      verifyCredentials,
     };
   },
 });
